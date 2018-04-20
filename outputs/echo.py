41edd6cfd09e0c2abdffc4ff7dc35d4d28f5d7cc
黄思铭 <huangsiming@yunnex.com>
2018-04-20 17:40:17 +0800

    add argument to example output

--- outputs/echo.py
+++ outputs/echo.py
@@ -2,5 +2,8 @@ from outputs import BaseOutput
 
 
 class Output(BaseOutput):
+    def __init__(self, prefix=''):
+        self.prefix = prefix
+
     def run(self, msg):
-        return msg
+        return self.prefix + msg
